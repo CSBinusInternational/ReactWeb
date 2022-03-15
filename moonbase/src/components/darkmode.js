7ec49b92a46e871441fbd0d3d49916f5901d34bd
Ardelia <71622783+ardeliaraminta@users.noreply.github.com>
2022-03-15 21:35:41 +0700

    try light switch

---
+++ moonbase/src/components/darkmode.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+function darkmode() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        Hello World
+        <button onClick={() => themeToggler()}> Switch Theme </button>
+      </StyledApp>
+    </ThemeProvider>
+  );
+}
+
+export default darkmode;
